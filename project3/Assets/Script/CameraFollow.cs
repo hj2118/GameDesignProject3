b287bc8963dea1bfc8554974f75b73a3eb483047
hj2118 <hj2118@nyu.edu>
2023-10-02 04:01:55 -0400

    camera fixed

--- project3/Assets/Script/CameraFollow.cs
+++ project3/Assets/Script/CameraFollow.cs
@@ -6,12 +6,21 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target;  // Reference to the player's Transform
     public float smoothSpeed = 0.125f;  // Smoothing factor for camera movement
-    public Vector3 offset;  // Offset from the player
+    public Vector3 offset = new Vector3(3, 1, -10);  // Offset from the player
+    Vector3 currentVelocity;
+
 
     void LateUpdate()
     {
         if (target != null)
         {
+            transform.position = Vector3.SmoothDamp(
+                transform.position,
+                target.position + offset,
+                ref currentVelocity,
+                smoothSpeed
+                );
+            /*
             // Calculate the desired camera position
             Vector3 desiredPosition = target.position + offset;
 
@@ -24,6 +33,7 @@ public class CameraFollow : MonoBehaviour
             // Debug log for position
             //Debug.Log("Desired Position: " + desiredPosition);
             //Debug.Log("Camera Position: " + transform.position);
+            */
         }
     }
 }
